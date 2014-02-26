7420681492fef638e8b27d6b87aaf5717a3317c1
Mark Ryder <ryddlemethis@gmail.com>
2014-02-26 21:23:12 +0000

    3rd commit

--- js/converter.js
+++ js/converter.js
@@ -1,6 +1,20 @@
+
 var Converter = (function() {
-	
+	//private
+	var converterValue = 2.2;
+	//public
 	return {
+		convertFromImperialToMetric: function(weight) {
+			var num = parseInt(weight);
+			if(isNaN(num)== true){
+				throw new Error ("Not a Number");
+			}
+			num = num / converterValue;
+			return Math.round(num);
+		},
+		convertFromMetricToImperial: function(weight) {
+			return weight / converterValue;
+		}
 		
 	}
 })()
